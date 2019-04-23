54e4093218c858b54ac291644337d2d183e529ae
core.editor <andreeavillatte@gmail.com>
2019-04-23 12:34:33 +0200

    après correction

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
     //si je clique sur le bouton
     $('#button').click(function () {
-    // je donne aux paragraphes firstText et thirdText une bordure de 5 pixels, verte à pointillés ("5px green dashed")
-    // il ne faut pas oublier de metre 'border' virgule et après le style
-    // aussi, quand on veut donner le style aux plusieurs elements, il faut mettre entre quotes simple les elements, separés par une virgule
+    // on ajoute une classe seulement au troisième paragraphe avec "addClass" 
     $('#thirdText').addClass('hide');
     // Et on relance le hide pour cacher le nouvel élément
     $('.hide').hide();
